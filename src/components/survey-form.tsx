841743b65b312cc18f92de1b986e2aab9013e26b
Solomon Zion <brainycodes.tech@gmail.com>
2025-04-29 09:22:21 +0100

    Changed mail format

--- src/components/survey-form.tsx
+++ src/components/survey-form.tsx
@@ -52,7 +52,7 @@ export default function SurveyForm() {
     const formData = new FormData(form);
 
     try {
-      const response = await fetch("https://getform.io/f/broymjda", {
+      const response = await fetch("https://getform.io/f/bwnqplva", {
         method: "POST",
         body: formData,
       });
