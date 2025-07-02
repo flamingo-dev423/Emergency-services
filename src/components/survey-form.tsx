00028f5ba04adea45c23f82a26716fb5feb194cc
Flamingo-Dev <Flamites021@outlook.com>
2025-07-02 01:37:45 -0700

    Change form

--- src/components/survey-form.tsx
+++ src/components/survey-form.tsx
@@ -52,7 +52,7 @@ export default function SurveyForm() {
     const formData = new FormData(form);
 
     try {
-      const response = await fetch("https://getform.io/f/bqomvgpb", {
+      const response = await fetch("https://getform.io/f/bvryldeb", {
         method: "POST",
         body: formData,
       });
